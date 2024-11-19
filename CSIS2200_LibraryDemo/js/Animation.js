01bffc230838e54e77e6e48b30ecec2a1557349f
JuliaXLiu <liux66@student.douglascollege.ca>
2024-11-18 21:10:22 -0800

    search page added

---
+++ CSIS2200_LibraryDemo/js/Animation.js
@@ -0,0 +1,22 @@
+function accordion(event){
+    let e = event.target
+    e.classList.toggle("active");
+    var panel = e.nextElementSibling;
+    
+    if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+        panel.style.padding = "0px 20px";
+    } else {
+    panel.style.maxHeight = panel.scrollHeight + "px";
+    panel.style.padding = "10px 20px";
+    }
+
+}
+
+window.onload = function() {
+    let eles = document.getElementsByTagName("button");
+    eles[1].click();
+    eles[2].click();
+    eles[3].click();
+    console.log(eles)
+};
